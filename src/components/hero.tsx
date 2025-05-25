4e3cb974ea9c3075b1abc08203f2768c0bb7de18
Resha Aditiya <reshaditiya@gmail.com>
2025-05-25 22:02:12 +0700

    fix(hero): change container to semantic html

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -8,13 +8,13 @@ export default function Hero({
   onSubmit?: (params: TLinkShortForm) => void;
 }) {
   return (
-    <div className="bg-primary text-primary-foreground relative space-y-4 overflow-hidden rounded-4xl p-10 lg:p-20">
+    <section className="bg-primary text-primary-foreground relative space-y-4 overflow-hidden rounded-4xl p-10 lg:p-20">
       <div>
         <h1 className="text-5xl font-black lg:text-7xl">cimit.</h1>
         <p>Shrink ‘em, save ‘em, and share ‘em—without the hassle.</p>
       </div>
       <HeroForm onSubmit={onSubmit} />
-      <Ripple effectClassName='top-[60px] left-[110px] lg:top-[118px] lg:left-[190px]' />
-    </div>
+      <Ripple effectClassName="top-[60px] left-[110px] lg:top-[118px] lg:left-[190px]" />
+    </section>
   );
 }
