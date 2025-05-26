65d268a8702439a90d3fa682e47d6b93b076a09e
Resha Aditiya <reshaditiya@gmail.com>
2025-05-26 21:07:27 +0700

    feat(footer): create footer component

---
+++ src/components/footer.tsx
@@ -0,0 +1,26 @@
+import Link from 'next/link';
+import CardContainer from './card-container';
+import Image from 'next/image';
+import { Globe } from 'lucide-react';
+
+export default function Footer({
+  personalDomain = `https://me.${process.env.NEXT_PUBLIC_DOMAIN_NAME}`,
+}: {
+  personalDomain?: string;
+}) {
+  return (
+    <CardContainer as="footer" className="flex items-center justify-between">
+      <span>
+        created by{' '}
+        <Link href={personalDomain} className="underline">
+          reshaditiya
+        </Link>
+        .
+      </span>
+      <div className="flex gap-2">
+        <Image src="./github.svg" alt="github" width={24} height={24} />
+        <Globe className="size-6" />
+      </div>
+    </CardContainer>
+  );
+}
---
+++ src/stories/components/footer.stories.tsx
@@ -0,0 +1,16 @@
+import Footer from '@/components/footer';
+import { Meta, StoryObj } from '@storybook/react';
+
+const meta = {
+  title: 'Components/Footer',
+  component: Footer,
+  tags: ['autodocs'],
+  args: {
+    personalDomain: `https://me.${process.env.NEXT_PUBLIC_DOMAIN_NAME}`,
+  },
+} satisfies Meta<typeof Footer>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
