8b67cb9714acf25e2416e701d47c6bbbe4506155
Resha Aditiya <reshaditiya@gmail.com>
2025-06-03 20:43:39 +0700

    feat(section-title): add the ability to add as prop

--- src/components/common/section-title.tsx
+++ src/components/common/section-title.tsx
@@ -1,13 +1,26 @@
 import { cn } from '@/lib/utils';
+import { ComponentPropsWithoutRef, ElementType } from 'react';
 
-export default function SectionTitle({
+type CardContainerProps<T extends ElementType> = {
+  as?: T;
+  className?: string;
+  children?: React.ReactNode;
+} & ComponentPropsWithoutRef<T>;
+
+export default function SectionTitle<T extends ElementType = 'span'>({
+  as,
   className,
   children,
   ...props
-}: React.ComponentProps<'span'>) {
+}: CardContainerProps<T>) {
+  const Component = as || 'span';
+
   return (
-    <span className={cn('text-xl font-bold lg:text-3xl', className)} {...props}>
+    <Component
+      className={cn('text-xl font-bold lg:text-3xl', className)}
+      {...props}
+    >
       {children}
-    </span>
+    </Component>
   );
 }
