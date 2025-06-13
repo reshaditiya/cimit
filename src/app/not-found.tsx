39b3601dc1feadeb7829aab8cca25887e7009d45
Resha Aditiya <reshaditiya@gmail.com>
2025-06-13 15:34:26 +0700

    fix(not-found): move text to translation and fix ripple responsiveness

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,19 +1,21 @@
 import { CardState } from '@/components/common/card-state';
 import { Ripple } from '@/components/magicui/ripple';
 import { Shell } from 'lucide-react';
+import { useTranslations } from 'next-intl';
 
 export default function NotFound() {
+  const t = useTranslations('not-found');
+
   return (
     <main className="flex h-svh items-center justify-center">
       <CardState
         icon={Shell}
         className="relative max-w-4xl"
-        title="what u lookin for???"
-        description="i can't give you something that doesn't exist, sort your
-          mind, get some fresh air, touch grass, relax, and try again."
+        title={t('title')}
+        description={t('description')}
       >
         <Ripple
-          effectClassName="top-[40px] lg:top-[182px]"
+          effectClassName="top-[40px] md:top-[96px] lg:top-[182px]"
           mainCircleSize={80}
         />
       </CardState>
