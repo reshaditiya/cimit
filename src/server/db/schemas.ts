4930cbdce4ebf8d6b8fa8e1fcc66654e76574dc3
Resha Aditiya <reshaditiya@gmail.com>
2025-06-13 23:09:48 +0700

    feat(db): add index to shortLink column

--- src/server/db/schemas.ts
+++ src/server/db/schemas.ts
@@ -1,12 +1,22 @@
-import { integer, pgTable, text, timestamp } from 'drizzle-orm/pg-core';
+import {
+  integer,
+  pgTable,
+  text,
+  timestamp,
+  uniqueIndex,
+} from 'drizzle-orm/pg-core';
 
-export const links = pgTable('links', {
-  shortLink: text('short_link').notNull().primaryKey(),
-  longLink: text('long_link').notNull(),
-  password: text('password'),
-  totalVisited: integer('total_visited').notNull().default(0),
-  createdBy: text('created_by'),
-  createdAt: timestamp('created_at').defaultNow().notNull(),
-  updatedAt: timestamp('updated_at').defaultNow().notNull(),
-  expiredAt: timestamp('expired_at'),
-});
+export const links = pgTable(
+  'links',
+  {
+    shortLink: text('short_link').notNull().primaryKey(),
+    longLink: text('long_link').notNull(),
+    password: text('password'),
+    totalVisited: integer('total_visited').notNull().default(0),
+    createdBy: text('created_by'),
+    createdAt: timestamp('created_at').defaultNow().notNull(),
+    updatedAt: timestamp('updated_at').defaultNow().notNull(),
+    expiredAt: timestamp('expired_at'),
+  },
+  (table) => [uniqueIndex('short_link_idx').on(table.shortLink)]
+);
