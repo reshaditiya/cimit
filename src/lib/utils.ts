6c038079b74e1ed19561a0f85124d9f6481431e4
Resha Aditiya <reshaditiya@gmail.com>
2025-06-14 11:01:36 +0700

    chore: install cryptr and crreate util

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,14 @@
 import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
+import Cryptr from 'cryptr';
+import { env } from './env';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+export function cryptr() {
+  const cryptr = new Cryptr(env.CRYPTR_SALT);
+
+  return cryptr;
 }
